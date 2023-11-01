efd886725592f4b37679d0276668e0648ee7381e
Md Nazari <mdnazari@vdi-scclab-021.lancs.local>
2023-11-01 12:57:57 +0000

    Added Mars but failed attempt to create the Asteroid Belt

--- TestDrive.java
+++ TestDrive.java
@@ -8,6 +8,9 @@ public class TestDrive {
         Planet venus = new Planet(120, 0, 20, "ORANGE", 0);
         Planet earth = new Planet(160, 0, 30, "BLUE", 0);
         Moon ourMoon = new Moon(25, 0, 7, "GRAY", 0, earth.distance, earth.angle);
+        Planet mars = new Planet(200, 0, 15, "ORANGE", 0);
+        Planet asteroid = new Planet(230, 0, 5, "GRAY", 0);
+        Planet[] asteroids = new Planet[50];
 
             while(true){
                 //The Sun
@@ -30,9 +33,21 @@ public class TestDrive {
                 
                 //Earth's Moon
                 ourMoon.drawPlanet(alamSemesta, ourMoon.distance, ourMoon.angle, ourMoon.diameter, ourMoon.colour, earth.distance, earth.angle);
-                //alamSemesta.drawSolarObjectAbout(ourMoon.distance, ourMoon.angle, ourMoon.diameter, ourMoon.colour, earth.distance, earth.angle);
                 ourMoon.rotate();
                 ourMoon.setSpeed(2);
+
+                // Mars
+                mars.drawPlanet(alamSemesta, mars.distance, mars.angle, mars.diameter, mars.colour, matahari.distance, matahari.angle);
+                mars.rotate();
+                mars.setSpeed(2);
+
+                // Asteroid Belt
+                for(int i = 0; i < 50; i++){
+                    asteroids[i].drawPlanet(alamSemesta, asteroid.distance, asteroid.angle, asteroid.diameter, asteroid.colour, matahari.distance, matahari.angle);
+                    asteroids[i].rotate();
+                    asteroids[i].setSpeed(5);
+                }
+
 
                 alamSemesta.finishedDrawing();
             }
